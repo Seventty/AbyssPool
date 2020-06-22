d43818af10c50f73931fa888108ef4a00e89444d
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-06-22 06:11:39 -0400

    init commit

---
+++ ICharServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AbyssPoolCH
+{
+    [ServiceContract]
+    public interface ICharServices
+    {
+
+    }
+
+    [DataContract]
+    public class ChatUser
+    {
+        private string userName, hostName, ipAddress;
+
+        [DataMember]
+        public string IpAddress { get => ipAddress; set => ipAddress = value; }
+        [DataMember]
+        public string HostName { get => hostName; set => hostName = value; }
+        [DataMember]
+        public string UserName { get => userName; set => userName = value; }
+    }
+
+}
