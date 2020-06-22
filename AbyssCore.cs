12cf40107bf9746fc6c6f840733fb689b0dd195b
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-06-22 07:33:26 -0400

    fast up

--- AbyssCore.cs
+++ AbyssCore.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace AbyssPoolCH
 {
+   
     public class AbyssCore
     {
+        private Dictionary<string, List<ChatMessage>> incomingMessages = new Dictionary<string, List<ChatMessage>>();
+        private List<ChatUser> connectedUsers = new List<ChatUser>();
 
+        public List<ChatUser> ConnectedUsers { get => connectedUsers; set => connectedUsers = value; }
+
+        public ChatUser addNewChatUser(ChatUser newUser)
+        {
+            var exists = from ChatUser e in this.connectedUsers where e.UserName == newUser.UserName select e;
+
+            if(exists.Count() == 0)
+            {
+
+            }
+        }
     }
 }
--- ChatService.cs
+++ ChatService.cs
@@ -7,8 +7,31 @@ using System.Text;
 
 namespace AbyssPoolCH
 {
-    public class ChatService
+    public class ChatService : ICharServices
     {
+        public ChatUser ClientConnect(string userName)
+        {
+            throw new NotImplementedException();
+        }
 
+        public List<ChatUser> GetAllusers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ChatMessage> GetNewMessages(ChatUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveChatUser(ChatUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendNewMessage(ChatMessage Newmessage)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
